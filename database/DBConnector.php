589b19845888cc8fe89d994ef64551eef1f5c80e
mrSemyon <syoma4k@gmail.com>
2022-06-12 17:07:42 +0500

    ConnectionDB refactoring

--- database/DBConnector.php
+++ database/DBConnector.php
@@ -2,19 +2,13 @@
 
 class DBConnector
 {
-    public static function make()
+    public static function make($config)
     {
-        $host = '127.0.0.1';
-        $db   = 'oop';
-        $user = 'root';
-        $pass = 'root';
-        $charset = 'utf8';
-        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-        $opt = [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
-        ];
-        return new PDO($dsn, $user, $pass, $opt);
+        return new PDO(
+            "mysql:host={$config['host']};dbname={$config['db']};charset={$config['charset']}",
+            $config['user'],
+            $config['pass'],
+            $config['opt']
+        );
     }
 }
--- database/start.php
+++ database/start.php
@@ -1,5 +1,7 @@
 <?php
+
 include $_SERVER['DOCUMENT_ROOT'] . '/database/QueryBuilder.php';
 include $_SERVER['DOCUMENT_ROOT'] . '/database/DBConnector.php';
+$config = include $_SERVER['DOCUMENT_ROOT'] . '/database/config.php';
 
-return new QueryBuilder(DBConnector::make());
+return new QueryBuilder(DBConnector::make($config['database']));
