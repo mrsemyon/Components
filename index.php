861de62e063b226471fa87995fbf4a4229d7f795
mrSemyon <syoma4k@gmail.com>
2022-11-21 06:06:28 +0500

    Added `Token` class

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <pre>
 <?php
+session_start();
+
 require $_SERVER['DOCUMENT_ROOT'] . '/Database.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/Config.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/Input.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/Validate.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/Token.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/Session.php';
 
 $GLOBALS['config'] = [
-    'mysql' => [
+    'mysql'     => [
         'host'      => '127.0.0.1',
         'name'      => 'components',
         'user'      => 'root',
@@ -18,33 +22,38 @@ $GLOBALS['config'] = [
             PDO::ATTR_EMULATE_PREPARES   => false,
         ],
     ],
+    'session'   => [
+        'tokenName' => 'token'
+    ]
 ];
 
 if (Input::exists()) {
-    $validate = new Validate();
+    if (Token::check(Input::get('token'))) {
+        $validate = new Validate();
 
-    $validation = $validate->check($_POST, [
-        'username' => [
-            'required'  => true,
-            'min'       => 2,
-            'max'       => 15,
-            'unique'    => 'users'
-        ],
-        'password' => [
-            'required'  => true,
-            'min'       => 3
-        ],
-        'password_again' => [
-            'required'  => true,
-            'matches'   => 'password'
-        ]
-    ]);
+        $validation = $validate->check($_POST, [
+            'username' => [
+                'required'  => true,
+                'min'       => 2,
+                'max'       => 15,
+                'unique'    => 'users'
+            ],
+            'password' => [
+                'required'  => true,
+                'min'       => 3
+            ],
+            'password_again' => [
+                'required'  => true,
+                'matches'   => 'password'
+            ]
+        ]);
 
-    if ($validation->passed()) {
-        echo 'passed';
-    } else {
-        foreach ($validation->errors() as $error) {
-            echo $error . '<br>';
+        if ($validation->passed()) {
+            echo 'passed';
+        } else {
+            foreach ($validation->errors() as $error) {
+                echo $error . '<br>';
+            }
         }
     }
 }
@@ -64,6 +73,7 @@ if (Input::exists()) {
         <label for="password_again">Password again</label>
         <input type="password" name="password_again">
     </div>
+    <input type="hidden" name="token" value="<?= Token::generate() ?>"> 
     <div class="field">
         <button type="submit">Submit</button>
     </div>
